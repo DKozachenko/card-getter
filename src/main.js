c79ca3e3c8c3621ec0b7f2a554afe7b7a679518f
Diana <kozachenkodiana03@mail.ru>
2023-06-04 20:51:21 +0700

    Moved to playing card file

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 'use strict'
 
 import { getRussianSuit, getRussianValue } from './utils.js';
+import { PlayingCard, createPlayingCard } from './playing-card.js';
 
 const state = new Proxy({
   queryParams: {
     cardNumber: 1,
+  },
+  viewParams: {
     translate: false
   },
   cards: []
@@ -18,17 +21,14 @@ const state = new Proxy({
       }
 
       for (let card of value) {
-        const playingCard = document.createElement('playing-card');
-
-        if (state.queryParams.translate) {
+        if (state.viewParams.translate) {
           card = {
             ...card,
             suit: getRussianSuit(card.suit),
             value: getRussianValue(card.value)
           }
-        } 
-        playingCard.setAttribute('card', JSON.stringify(card));
-        contentList.append(playingCard);
+        }
+        contentList.append(createPlayingCard(card));
       }
     }
 
@@ -37,62 +37,8 @@ const state = new Proxy({
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  const template = document.querySelector('template[name="playing-card"]');
-  class PlayingCard extends HTMLElement {
-    constructor() {
-      super();
-      const component = template.content.children[0].cloneNode(true);
-      this.attachShadow({ mode: 'open' }).appendChild(component);
-
-      const style = document.createElement("style");
-      style.textContent = `.card {
-        width: 100%;
-        max-width: 300px;
-        margin-bottom: 10px;
-      }
-      
-      .card__image {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      
-        max-height: 300px;
-        overflow: hidden;
-      }
-      
-      .card__image img {
-        display: block;
-        height: auto;
-        width: 250%;
-      }`;
-      const linkElem = document.createElement('link');
-      linkElem.setAttribute('rel', 'stylesheet');
-      linkElem.setAttribute('href', 'https://unpkg.com/98.css');
-      this.shadowRoot.append(style, linkElem);
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-      if (name === 'card') {
-        const card = JSON.parse(this.getAttribute(name));
-        const title = this.shadowRoot.querySelector('.title-bar-text');
-        const bodyP = this.shadowRoot.querySelector('.window-body p');
-        const bodyImg = this.shadowRoot.querySelector('.window-body .card__image img');
-
-        title.textContent = `Карта ${card.code}`;
-        bodyP.textContent = `${card.value} ${card.suit}`;
-        bodyImg.setAttribute('alt', card.code);
-        bodyImg.setAttribute('src', card.images.svg);
-      }
-    }
-
-    static get observedAttributes() {
-      return ['card'];
-    }
-  }
-
-  const componentName = template.getAttribute('name');
   customElements.define(
-    componentName,
+    'playing-card',
     PlayingCard 
   );
 
@@ -118,6 +64,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   cardTranslate.addEventListener('click', (e) => {
     const value = !e.target.previousElementSibling.checked;
-    state.queryParams.translate = value;
+    state.viewParams.translate = value;
   });
 });
---
+++ src/playing-card.js
@@ -0,0 +1,58 @@
+export class PlayingCard extends HTMLElement {
+  constructor() {
+    super();
+    const template = document.querySelector('template[name="playing-card"]');
+    const component = template.content.children[0].cloneNode(true);
+    this.attachShadow({ mode: 'open' }).appendChild(component);
+
+    const style = document.createElement("style");
+    style.textContent = `.card {
+      width: 100%;
+      max-width: 300px;
+      margin-bottom: 10px;
+    }
+    
+    .card__image {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    
+      max-height: 300px;
+      overflow: hidden;
+    }
+    
+    .card__image img {
+      display: block;
+      height: auto;
+      width: 250%;
+    }`;
+    const linkElem = document.createElement('link');
+    linkElem.setAttribute('rel', 'stylesheet');
+    linkElem.setAttribute('href', 'https://unpkg.com/98.css');
+    this.shadowRoot.append(style, linkElem);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'card') {
+      const card = JSON.parse(this.getAttribute(name));
+      const title = this.shadowRoot.querySelector('.title-bar-text');
+      const bodyP = this.shadowRoot.querySelector('.window-body p');
+      const bodyImg = this.shadowRoot.querySelector('.window-body .card__image img');
+
+      title.textContent = `Карта ${card.code}`;
+      bodyP.textContent = `${card.value} ${card.suit}`;
+      bodyImg.setAttribute('alt', card.code);
+      bodyImg.setAttribute('src', card.images.svg);
+    }
+  }
+
+  static get observedAttributes() {
+    return ['card'];
+  }
+}
+
+export function createPlayingCard(card) {
+  const playingCard = document.createElement('playing-card');
+  playingCard.setAttribute('card', JSON.stringify(card));
+  return playingCard;
+}
